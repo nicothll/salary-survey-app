a4a72f6812c7bd5affd1141b819fc6d511f5a83b
Nicolas Thuillier <nicothll.dev@gmail.com>
2022-08-30 19:00:38 -0500

    Updated traininglab and saved new ml_steps

--- app/explore_page.py
+++ app/explore_page.py
@@ -16,11 +16,9 @@ df = load_data()
 def show():
     st.title("Explore Software Engineer Salaries")
 
-    st.write(
-        """
-    ### Stack Overflow Developer Survey 2022    
-    """
-    )
+    st.subheader("Stack Overflow Developer Survey 2022")
+
+    st.write("#### Countries who responded to the survey")
 
     data = df["Country"].value_counts()
 
@@ -29,23 +27,23 @@ def show():
     fig = go.Figure(data=[go.Pie(labels=data.index, values=data, hole=0, pull=explode)])
     fig.update_layout(height=600, width=600)
 
-    st.write(
-        """
-    #### Countries who responded to the survey
-    """
-    )
-
     st.plotly_chart(fig, use_container_width=True)
 
-    data = df.groupby(["Country"]).mean()
+    st.write("#### Average Salary by Country (in USD)")
 
+    data = df.groupby(["Country"]).mean()
     fig2 = px.bar(data, y="Salary", x=data.index, text_auto=".2s")
 
     fig2.update_layout(height=600, width=900)
 
-    st.write(
-        """
-    #### Average Salary by Country (in USD)
-    """
-    )
     st.plotly_chart(fig2, use_container_width=True)
+
+    st.write("#### Average Salary by Experience")
+
+    data = df.groupby(["YearsCodePro"]).mean()
+    fig3 = px.line(data, y="Salary", x=data.index)
+
+    fig3.data[0].line.color = "#7bf542"
+    fig3.update_layout(height=600, width=900)
+
+    st.plotly_chart(fig3, use_container_width=True)
